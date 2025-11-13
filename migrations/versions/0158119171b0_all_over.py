62775673f0cb6389cca24e9cf3678f89e504314b
Sewanu Isaiah <sewanu708@gmail.com>
2025-11-13 02:07:01 +0100

    db

--- migrations/versions/0158119171b0_all_over.py
+++ migrations/versions/0158119171b0_all_over.py
@@ -1,8 +1,8 @@
-"""initial migration
+"""all over
 
-Revision ID: 6a3c0bb05412
+Revision ID: 0158119171b0
 Revises: 
-Create Date: 2025-11-12 00:44:23.083585
+Create Date: 2025-11-13 02:06:01.091940
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6a3c0bb05412'
+revision: str = '0158119171b0'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -25,7 +25,7 @@ def upgrade() -> None:
     sa.Column('id', sa.UUID(), nullable=False),
     sa.Column('is_admin', sa.Boolean(), nullable=True),
     sa.Column('email', sa.String(), nullable=False),
-    sa.Column('password', sa.String(), nullable=False),
+    sa.Column('password_hash', sa.String(), nullable=False),
     sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
     sa.Column('updated_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
     sa.PrimaryKeyConstraint('id'),
