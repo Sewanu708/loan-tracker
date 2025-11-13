fbd0171386b18ecbf45a037d215a71c1134fe298
Sewanu Isaiah <sewanu708@gmail.com>
2025-11-13 01:07:16 +0100

    db url

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import  sessionmaker
 from sqlalchemy.engine import create_engine
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 db_url = os.getenv("DATABASE_URL")
+if not db_url:
+    raise ValueError("No DATABASE_URL set for the application")
+
+if db_url.startswith("postgresql://"):
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
+    
 print("This is your database url", db_url)
 
 engine = create_engine(db_url)
